86b560e1b7f0250e88ea3c8fd7257d42cf48f569
Stuart Baker <sfb28@duke.edu>
2019-04-15 15:22:48 -0400

    lightweight relay server, write send_request for client server

--- relay_server.py
+++ relay_server.py
@@ -1,6 +1,10 @@
 import sys
 import socket
 
+
+def main():
+    listen()
+
 def listen():
     serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     serversocket.bind(('localhost', 5000))
@@ -9,7 +13,13 @@ def listen():
         clientsocket, address = serversocket.accept()
         payload = clientsocket.recv(4096).decode('utf-8')
 
+        clientsocket.close()
+
+    return
 
 def deserialize_payload(payload):
     encrypted_message, AES_key = payload.split(b'###')
-    print(encrypted_message)
+    print(encrypted_message, AES_key)
+
+if __name__ == '__main__':
+    main()
