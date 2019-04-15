db99896270d0e7d06f85d9dcd00ebeca394e643c
Jeffery Liu <jeffery300@gmail.com>
2019-04-15 14:41:25 -0400

    crypto.py

--- crypto.py
+++ crypto.py
@@ -1,1 +1,14 @@
+from cryptography.fernet import Fernet
 
+def genSymKey():
+    symKey = Fernet(Fernet.generate_key())
+    return symKey
+
+key1 = genSymKey()
+key2 = genSymKey()
+
+enc1 = key1.encrypt(b"my deep dark secret")
+enc2 = key2.encrypt(enc1)
+print(enc2)
+pltext = key1.decrypt(key2.decrypt(enc2))
+print(pltext)
